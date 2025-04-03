5f8c0068de65794149f1d54da9b4e40074b0ba51
Yun DongGeun <ydk1104@naver.com>
2025-04-03 10:00:50 +0900

    애니메이션 간단 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { 가사 } from "./TBH";
 import { useState } from "react";
+import { delay, motion } from "framer-motion";
 
 export default function Home() {
   const 소절 = 가사.split("\n");
@@ -29,11 +30,56 @@ export default function Home() {
             {isplay ? "■" : "▶"}
           </button>
           {소절.map((v, i) => {
+            let flag = false;
             return (
-              <div className="font-bold text-gray-400" key={`${v}${i}`}>
+              <div key={i}>
+                {v.split("").map((w, j) => {
+                  if (w === "\\") {
+                    flag = !flag;
+                    return <></>;
+                  }
+                  return (
+                    <motion.span
+                      className="font-bold text-gray-400"
+                      variants={{
+                        first: { color: "gray" },
+                        animateEnd: {
+                          color: "#0000cc",
+                          background: flag ? "#7777cc" : "white",
+                        },
+                      }}
+                      initial="first"
+                      animate="animateEnd"
+                      transition={{
+                        delay: i + j / v.length,
+                      }}
+                      key={`${i}.${j}`}
+                    >
+                      {w}
+                    </motion.span>
+                  );
+                })}
+              </div>
+            );
+            return (
+              <motion.div
+                className="font-bold text-gray-400"
+                key={`${v}${i}`}
+                variants={{
+                  first: { color: "gray" },
+                  animateEnd: { color: "blue" },
+                }}
+                initial="first"
+                animate="animateEnd"
+                transition={{
+                  ease: "linear",
+                  duration: 1, // 애니메이션이 총 걸리는 시간
+                  delay: i, // 처음 애니메이션 delay
+                }}
+              >
                 &nbsp;
                 {v}
-              </div>
+              </motion.div>
             );
           })}
         </div>
