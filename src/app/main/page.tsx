0e758ed693d36c6ba72651fb09ccee159a7a99e7
Yun DongGeun <ydk1104@naver.com>
2025-04-26 18:11:30 +0900

    메인페이지 간단히

---
+++ src/app/main/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+const Header = () => {
+  return <div className="flex"></div>;
+};
+
+const Footer = () => {
+  return <div className="flex"></div>;
+};
+
+export default function main() {
+  return (
+    <div className="p-12">
+      <Header />
+      <div className=" text-3xl">소개</div>
+      <div className="align-middle">qwer 비공식 콜표 사이트입니다.</div>
+      <div className=" text-3xl">앨범 목록</div>
+      <div>Harmony from Discord</div>
+      <div>MANITO</div>
+      <div>Algorithm’s Blossom </div>
+      <div className=" text-3xl">노래 목록</div>
+      <div>Discord</div>
+      <div>고민중독</div>
+      <div>내 이름 맑음</div>
+      <Footer />
+    </div>
+  );
+}
